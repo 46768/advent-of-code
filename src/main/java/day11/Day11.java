c721ccf900543694c38893f01d140467747afda5
46768 <46768@bodin.ac.th>
2024-12-11 20:03:13 +0700

    Got rid of most maven warnings, Day 11 finished using recursion

--- src/main/java/day11/Day11.java
+++ src/main/java/day11/Day11.java
@@ -1,6 +1,10 @@
 package day11;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.ArrayDeque;
+import java.util.HashMap;
+import java.util.HashSet;
 
 import dayBase.DayBase;
 import input.InputData;
@@ -23,50 +27,51 @@ public class Day11 extends DayBase<ArrayList<Long>> {
 		return stones;
 	}
 
-	private ArrayList<Long> transformStone(long stone) {
-		ArrayList<Long> transformedStone = new ArrayList<>();
+	private List<Long> transformStone(long stone) {
 		if (stone == 0) {
-			transformedStone.add((long)1);
+			return List.of((long)1);
 		} else if (Long.toString(stone).length() % 2 == 0) {
-			String stringgedStone = Long.toString(stone);
-			String firstHalf = stringgedStone.substring(0, (stringgedStone.length()/2));
-			String secondHalf = stringgedStone.substring(stringgedStone.length()/2);
-			transformedStone.add(Long.parseLong(firstHalf));
-			transformedStone.add(Long.parseLong(secondHalf));
-		} else {
-			transformedStone.add(stone*2024);
+			long stoneLen = (long)Math.log10(stone)+1;
+			long stonePow = (long)Math.pow(10, stoneLen/2);
+			long firstHalf = Math.floorDiv(stone, stonePow);
+			long secondHalf = stone % stonePow;
+			return List.of(firstHalf, secondHalf);
+		}
+		return List.of(stone*2024);
+	}
+
+	private long blinkStone(long stone, int count, HashMap<Integer, HashMap<Long, Long>> memo) {
+		if (count == 0) {
+			return (long)1;
+		}
+		if (memo.getOrDefault(count, HashMap.newHashMap(0)).containsKey(stone)) return memo.get(count).get(stone);
+
+		long cnt = 0;
+		List<Long> blinks = transformStone(stone);
+		for (long blinkStone : blinks) {
+			cnt += blinkStone(blinkStone, count-1, memo);
 		}
 
-		return transformedStone;
+		memo.putIfAbsent(count, new HashMap<>());
+		memo.get(count).putIfAbsent(stone, cnt);
+		return cnt;
 	}
 
 	public void part1() {
-		@SuppressWarnings("unchecked")
-		ArrayList<Long> dataClone = (ArrayList<Long>)data.clone();
-
-		for (int i = 0; i < 25; i++) {
-			ArrayList<Long> newData = new ArrayList<>();
-			for (Long stone : dataClone) {
-				newData.addAll(transformStone(stone));	
-			}
-			dataClone = newData;
+		HashMap<Integer, HashMap<Long, Long>> memos = new HashMap<>();
+		long totalStones = 0;
+		for (long stone : data) {
+			totalStones += blinkStone(stone, 25, memos);
 		}
-
-		Logger.log("Stone counts: %d", dataClone.size());
+		Logger.log("Stone counts w 25 blinks: %d", totalStones);
 	}
 
 	public void part2() {
-		@SuppressWarnings("unchecked")
-		ArrayList<Long> dataClone = (ArrayList<Long>)data.clone();
-
-		for (int i = 0; i < 75; i++) {
-			ArrayList<Long> newData = new ArrayList<>();
-			for (Long stone : dataClone) {
-				newData.addAll(transformStone(stone));	
-			}
-			dataClone = newData;
+		HashMap<Integer, HashMap<Long, Long>> memos = new HashMap<>();
+		long totalStones = 0;
+		for (long stone : data) {
+			totalStones += blinkStone(stone, 75, memos);
 		}
-
-		Logger.log("Stone counts: %d", dataClone.size());
+		Logger.log("Stone counts w 75 blinks: %d", totalStones);
 	}
 }
