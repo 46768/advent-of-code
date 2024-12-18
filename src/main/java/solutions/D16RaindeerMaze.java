6107fc0a45c4492e86fa64d251a39553205255b3
46768 <46768@bodin.ac.th>
2024-12-18 20:09:30 +0700

    Working solution to part 1 day 16

--- src/main/java/solutions/D16RaindeerMaze.java
+++ src/main/java/solutions/D16RaindeerMaze.java
@@ -2,8 +2,10 @@ package solutions;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.PriorityQueue;
 import java.util.Comparator;
+import java.util.Collections;
 
 import logger.Logger;
 import dayBase.DayBase;
@@ -28,8 +30,9 @@ class AStar {
 	public ArrayList<Coord> pathfind(Coord start, Coord end) {
 		// AStar variables
 		HashMap<Coord, Coord> coordSrc = new HashMap<>();
-		HashMap<Coord, Integer> gCost = new HashMap<>();
+		HashMap<Coord, Long> gCost = new HashMap<>();
 
+		HashSet<Coord> visited = new HashSet<>();
 		PriorityQueue<Double[]> queue = new PriorityQueue<>(new Comparator<Double[]>() {
 			@Override
 			public int compare(Double[] o1, Double[] o2) {
@@ -40,23 +43,49 @@ class AStar {
 
 		// Initialization
 		coordSrc.put(start, start);
-		gCost.put(start, 0);
-		queue.add(new Double[]{(double)map.compressCoord(start), h.run(start, end), 0d});
+		gCost.put(start, 0l);
+		queue.add(new Double[]{h.run(start, end), (double)map.compressCoord(start), (double)map.compressCoord(Coord.withY(1))});
 
 		while (queue.size() > 0) {
 			Double[] queueElement = queue.remove();
 			Coord elementCoord = map.decompressCoord((long)(double)queueElement[1]);
+			Coord direction = map.decompressCoord((long)(double)queueElement[2]);
+			Long currentGCost = gCost.get(elementCoord);
+			visited.add(elementCoord);
 			if (elementCoord.equals(end)) break;
 
 			for (Coord neighbor : elementCoord.getSurroundingCoord(false)) {
 				if (map.getVal(neighbor) == '.') {
+					Coord facingDirection = neighbor.subtract(elementCoord);
+					Coord directionProduct = direction.multiply(facingDirection);
+					Long neighborGCost = ((directionProduct.x() + directionProduct.y()) == 0 ? 1+penalty : 1)+currentGCost;
+
+					if (neighborGCost < gCost.getOrDefault(neighbor, Long.MAX_VALUE) && !visited.contains(neighbor)) {
+						coordSrc.put(neighbor, elementCoord);
+						gCost.put(neighbor, neighborGCost);
+						queue.add(new Double[]{
+							(double)neighborGCost+h.run(neighbor, end),
+							(double)map.compressCoord(neighbor),
+							(double)map.compressCoord(facingDirection)
+						});
+					}
+				} else if (map.getVal(neighbor) == 'E') {
+					coordSrc.put(neighbor, elementCoord);
+					queue.clear();
+					break;
 				}
 			}
 		}
 
-
 		// Build the array of coord used to take from start to end
 		ArrayList<Coord> returnArray = new ArrayList<>();
+		Coord currentCoord = end;
+		while (!currentCoord.equals(start)) {
+			returnArray.add(currentCoord);
+			currentCoord = coordSrc.get(currentCoord);
+		}
+		returnArray.add(start);
+		Collections.reverse(returnArray);
 
 		return returnArray;
 	}
@@ -85,12 +114,53 @@ public class D16RaindeerMaze extends DayBase<Grid<Character>> {
 				}
 			}
 		}
-		map.printCurrentState();
-		Logger.debug(startPos);
-		Logger.debug(endPos);
 		return map;
 	}
 
-	public void part1() {}
+	private double heuristic(Coord start, Coord end, int cost) {
+		long diffX = start.x()-end.x();
+		long diffY = start.y()-end.y();
+		double dist = Math.sqrt(diffX*diffX + diffY*diffY);
+		return cost*(diffY/dist) + dist - (cost/100)*(diffX/dist);
+	}
+
+	private long calculateScore(ArrayList<Coord> path) {
+		long score = 0;
+		Coord direction = new Coord(0, 1);
+		for (int i = 0; i < path.size()-1; i++) {
+			Coord current = path.get(i);
+			Coord next = path.get(i+1);
+			Coord dir = next.subtract(current);
+			if (!direction.equals(dir)) {
+				direction = dir;
+				score += 1001;
+			} else {
+				score += 1;
+			}
+		}
+		return score;
+	}
+
+	public void part1() {
+		int turnCost = 1000;
+		AStar searcher = new AStar((s, e) -> heuristic(s, e, turnCost), turnCost, data);
+		ArrayList<Coord> path = searcher.pathfind(startPos, endPos);
+		
+		// debug
+		for (int x = 0; x < data.sizeX(); x++) {
+			for (int y = 0; y < data.sizeY(); y++) {
+				Coord coord = new Coord(x, y);
+				if (path.contains(coord)) {
+					Logger.print('O');
+				} else {
+					Logger.print(data.getVal(coord));
+				}
+			}
+			Logger.print('\n');
+		}
+
+		// Score calculation
+		Logger.log("Score for map: %d", calculateScore(path));
+	}
 	public void part2() {}
 }
